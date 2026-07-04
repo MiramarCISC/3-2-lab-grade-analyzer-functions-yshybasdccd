c089d739b4c9e5af19533e866aa256281394dc93
aparsons001 <aparsons001@student.sdccd.edu>
2026-07-04 19:13:10 +0000

    updated todo's, commented code, tested and passed

--- src/grade_analyzer.cpp
+++ src/grade_analyzer.cpp
@@ -4,35 +4,46 @@
 using namespace std;
 
 double calculateTotal(double score1, double score2, double score3) {
-    // TODO: Return the sum of the three scores.
-    return 0.0;
+    return  score1 + score2 + score3; //returns scores added together
 }
 
-double calculateAverage(double total, int count) {
-    // TODO:
-    // If count is less than or equal to 0, return 0.0.
-    // Otherwise, return total divided by count.
-    return 0.0;
+double calculateAverage(double total, int count){
+    if (count <= 0){ 
+        // checks if the count is less than or equal to 0
+        return 0.0;
+    } else {
+    return total / count; // otherwise, returns total divided by count
+    }
 }
 
 char determineLetterGrade(double average) {
-    // TODO:
-    // Return 'A' for averages 90 or higher.
-    // Return 'B' for averages 80 or higher.
-    // Return 'C' for averages 70 or higher.
-    // Return 'D' for averages 60 or higher.
-    // Return 'F' otherwise.
-    return 'F';
+    if (average >= 90) { // if avg greater than (>) or equal to (=) (ex >=) 90, show A
+        return 'A';
+    } else if (average >= 80){ // if avg >= 80, show B
+        return 'B';
+    } else if (average >= 70){ // if avg >= 70, show C
+        return 'C';
+    } else if (average >= 60){ // if avg >= 60, show D
+        return 'D';
+    } else{
+        return 'F'; // else, return F
+    }
 }
 
 bool isPassing(double average) {
-    // TODO: Return true when average is 70 or higher.
-    return false;
+    if (average >= 70){ // checks if average is greater than or equal to 70 and returns true
+        return true;
+    } else {
+    return false; // any other condition will return false
+    }
 }
 
 bool isValidScore(double score) {
-    // TODO: Return true when score is between 0 and 100, inclusive.
-    return false;
+    if (score >= 0 && score <= 100){
+        return true; // checks if score is greater than 0 and less than 100
+    } else {
+    return false; // anything else returns false
+    }
 }
 
 void printGradeReport(double total, double average, char letterGrade, bool passing) {
